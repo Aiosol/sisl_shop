c5596a1a4e2a852df62bfc32353bb33f984550b1
Aiosol <infodomhost@gmail.com>
2025-02-25 15:15:34 +0600

    req file added

--- SISL Mitsubishi eShop/my_eshop_project/passenger_wsgi.py
+++ SISL Mitsubishi eShop/my_eshop_project/passenger_wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add your project directory to sys.path
-path = '/home/mitsubishifabd/apps/sisl_shop/SISL Mitsubishi eShop/my_eshop_project'
+path = '/home/mitsubishifabd/apps/sisl_shop/SISL Mitsubishi eShop\my_eshop_project'
 if path not in sys.path:
     sys.path.insert(0, path)
 
